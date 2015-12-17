5c33f1d353e94a25578a32f7ea06a4c549989ce7
Dounm_DigitalOcean <niuchong893184@gmail.com>
2015-12-16 23:58:29 -0500

    monitor the connected site of every ip

--- shadowsocks_statistic/main.py
+++ shadowsocks_statistic/main.py
@@ -36,28 +36,41 @@ def grep_logfile(log_file, cur_day):
 
 def extract_connected_ips(lines):
     ip_dict = {}
+    domain_set = set(['com', 'edu', 'org'])
     for line in lines:
+        #get the specific ip
         pos1 = line.find('from')
         pos2 = line.find(':', pos1)
         ip = line[pos1+5:pos2]
+        #get the destination
+        tmp = line[line.find('connecting')+10 : line.find('from')]
+        tmp = tmp.split('.')
+        dest = tmp[len(tmp)-2]
+        if dest in domain_set:
+            dest = tmp[len(tmp)-3]
+
         if ip in ip_dict:
-            ip_dict[ip] += 1
+            ip_dict[ip].add(dest) 
         else:
-            ip_dict[ip] = 1
+            ip_dict[ip] = set([dest])
     del ip_dict['']
+    print ip_dict
     return ip_dict
 
 def send_email(ip_dict, config_file, cur_day):
     subject = 'The connections to shadowsocks server of ' + cur_day
     msg = '\n'
     for k in ip_dict:
-        msg += '[%s]' % k + ' connected ' + str(ip_dict[k]) + ' times in ' + cur_day + '\n'
+        msg += '[%s]' % k + ' has connected the sites below in %s:\n\t' % cur_day
+        for domain in ip_dict[k]:
+            msg += domain + ', '
+        msg += '\n'
+        msg += '\n'
     msg += '\n'
 
     d = dict()
     with open(config_file, 'r') as f:
         d = json.load(f)
-        print d
     util.email_sender.send_email(str(d['sender']), str(d['sender_pwd']), \
         str(d['receiver']), subject, msg)
 
@@ -67,5 +80,4 @@ config_file = sys.argv[2]
 cur_day = get_cur_day()
 lines_list = grep_logfile(log_file, cur_day)
 ip_dict = extract_connected_ips(lines_list)
-print ip_dict
 send_email(ip_dict, config_file, cur_day)
